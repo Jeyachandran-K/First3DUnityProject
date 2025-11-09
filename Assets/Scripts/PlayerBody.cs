fba58cdf80c55df8ee3913b179cc64b3a98ce018
Jeyachandran <jeyachandran.work@gmail.com>
2025-11-10 03:22:21 +0530

    PlayerTipOver fixed
    just freeze the rotation along all axis and the character never tips over.

--- Assets/Scripts/PlayerBody.cs
+++ Assets/Scripts/PlayerBody.cs
@@ -3,37 +3,43 @@ using UnityEngine.InputSystem;
 
 public class PlayerBody : MonoBehaviour
 {
-    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private Rigidbody playerRigidBody;
     [SerializeField] private float forceAmount;
     [SerializeField] private float lateralForceAmount;
-   
+
+    void Start()
+    {
+        
+    }
+
+
     void Update()
     {
         if (Keyboard.current.upArrowKey.isPressed)
         {
             //transform.position += new Vector3(0, 0, 1);
             //rigidbody.AddForce(Vector3.forward * forceAmount);
-            rigidbody.linearVelocity = new Vector3(0, 0, 1) * forceAmount;
+            playerRigidBody.linearVelocity = new Vector3(0, 0, 1) * forceAmount;
         }
         if (Keyboard.current.downArrowKey.isPressed)
         {
             //transform.position += new Vector3(0, 0, -1);
-            rigidbody.linearVelocity = new Vector3(0, 0, -1) * forceAmount;
+            playerRigidBody.linearVelocity = new Vector3(0, 0, -1) * forceAmount;
         }
         if (Keyboard.current.spaceKey.isPressed)
         {
             //transform.position += new Vector3(0, 1, 0);
-            rigidbody.linearVelocity = new Vector3(0, 1, 0) * forceAmount;
+            playerRigidBody.linearVelocity = new Vector3(0, 1, 0) * forceAmount;
         }
         if (Keyboard.current.rightArrowKey.isPressed)
         {
             //transform.position += new Vector3(1, 0, 0);
-            rigidbody.linearVelocity = new Vector3(1, 0, 0) * lateralForceAmount;
+            playerRigidBody.linearVelocity = new Vector3(1, 0, 0) * lateralForceAmount;
         }
         if (Keyboard.current.leftArrowKey.isPressed)
         {
             //transform.position += new Vector3(-1, 0, 0);
-            rigidbody.linearVelocity = new Vector3(-1, 0, 0) * lateralForceAmount;
+            playerRigidBody.linearVelocity = new Vector3(-1, 0, 0) * lateralForceAmount;
         }
     }
 }
