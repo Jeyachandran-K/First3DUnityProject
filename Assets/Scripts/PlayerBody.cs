ed37b8c738902bb058ce00cfc7c7d46fb9c084ae
Jeyachandran <jeyachandran.work@gmail.com>
2025-11-09 13:02:13 +0530

    MovementCode update

--- Assets/Scripts/PlayerBody.cs
+++ Assets/Scripts/PlayerBody.cs
@@ -4,28 +4,36 @@ using UnityEngine.InputSystem;
 public class PlayerBody : MonoBehaviour
 {
     [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private float forceAmount;
+    [SerializeField] private float lateralForceAmount;
    
     void Update()
     {
         if (Keyboard.current.upArrowKey.isPressed)
         {
-            rigidbody.transform.position += new Vector3(0, 0, 1);
+            //transform.position += new Vector3(0, 0, 1);
+            //rigidbody.AddForce(Vector3.forward * forceAmount);
+            rigidbody.linearVelocity = new Vector3(0, 0, 1) * forceAmount;
         }
         if (Keyboard.current.downArrowKey.isPressed)
         {
-            rigidbody.transform.position += new Vector3(0, 0, -1);
+            //transform.position += new Vector3(0, 0, -1);
+            rigidbody.linearVelocity = new Vector3(0, 0, -1) * forceAmount;
         }
         if (Keyboard.current.spaceKey.isPressed)
         {
-            rigidbody.transform.position += new Vector3(0, 1, 0);
+            //transform.position += new Vector3(0, 1, 0);
+            rigidbody.linearVelocity = new Vector3(0, 1, 0) * forceAmount;
         }
         if (Keyboard.current.rightArrowKey.isPressed)
         {
-            rigidbody.transform.position += new Vector3(1, 0, 0);
+            //transform.position += new Vector3(1, 0, 0);
+            rigidbody.linearVelocity = new Vector3(1, 0, 0) * lateralForceAmount;
         }
         if (Keyboard.current.leftArrowKey.isPressed)
         {
-            rigidbody.transform.position += new Vector3(-1, 0, 0);
+            //transform.position += new Vector3(-1, 0, 0);
+            rigidbody.linearVelocity = new Vector3(-1, 0, 0) * lateralForceAmount;
         }
     }
 }
