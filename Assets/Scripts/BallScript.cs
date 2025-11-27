b29e980c5b3cb7cfecef9b71261ed684883ab046
Jeyachandran <jeyachandran.work@gmail.com>
2025-11-27 10:38:00 +0530

    xtest scene added

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = new Vector3(15,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = new Vector3(15, 0, 0);
+    }
+}
